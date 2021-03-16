91aad3637bc89aba813e83caad2c47af3eab0d25
Manohar Singh <manoharsinghrawat@cedcoss.com>
2021-03-16 09:55:51 +0530

    initial

---
+++ CocoaTest/CocoaTest.h
@@ -0,0 +1,18 @@
+//
+//  CocoaTest.h
+//  CocoaTest
+//
+//  Created by Manohar Singh Rawat on 15/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoaTest.
+FOUNDATION_EXPORT double CocoaTestVersionNumber;
+
+//! Project version string for CocoaTest.
+FOUNDATION_EXPORT const unsigned char CocoaTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaTest/PublicHeader.h>
+
+
---
+++ CocoaTest/TestWindow.swift
@@ -0,0 +1,14 @@
+//
+//  TestWindow.swift
+//  CocoaTest
+//
+//  Created by Manohar Singh Rawat on 15/03/21.
+//
+
+import Foundation
+public class Variation{
+    public init(){}
+    public func display(){
+        print("new pods")
+    }
+}
